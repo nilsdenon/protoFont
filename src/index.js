34ddae812f0f5ada319e870c8ddfc7d5272a129e
Damien Pierre <kalumbox@gmail.com>
2019-02-27 15:34:09 +0100

    latest changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './scss/main.scss';
-
-
-
-// Allge Google Webfons aus dem API = sehr lange JSON Datei
-var gf = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBcJ0zfjh1BYgpDtpXDmigTl-53aojQ-Wc";
-
-// alle Webfonts im Select Element laden
-function loadWebFonts() {
-
-}
-
